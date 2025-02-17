14c2f1eee36c4410e6391b660ab975d9776b61bf
NabilHadd <nabil.haddad@alumnos.ucn.cl>
2025-02-17 01:41:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 function openLightbox(imgElement) {
     const lightbox = document.getElementById('lightbox');
     const lightboxImg = document.getElementById('lightbox-img');
-    const lightboxDescription = document.getElementById('lightbox-description');
-
-    // Set the source of the lightbox image
+    
     lightboxImg.src = imgElement.src;
-
-    // Set the description text (or modify this as needed)
-    lightboxDescription.textContent = 'Descripción'; // Puedes cambiar la descripción aquí
-
-    // Display the lightbox
     lightbox.style.display = 'flex';
 }
-
 function closeLightbox() {
     const lightbox = document.getElementById('lightbox');
     lightbox.style.display = 'none';
